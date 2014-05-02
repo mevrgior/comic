5c0573426100c97f89ac3208ebe45612d223198c
mevrgior <mevrgior@googlemail.com>
2014-05-02 17:14:47 +0200

    signup route

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,45 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) {" Comic Dreams "}
+	
+
+  subject { page }
 
   describe "Home page" do
+    before {visit root_path}
 
-  	it "should have the right title base " do
-  		visit root_path
-  		expect(page).to have_title ("#{base_title}")
-  	end
+    it { should have_title (full_title(''))}
 
-    it "should have the content 'Sample App'" do
-    	visit root_path
-    	expect(page).to have_content ('Sample App')
+    it { should have_content ('Sample App')}
 
-    end
   end
+
+
 
   describe "Help page" do
 
-  
+   before {visit help_path}
 
-    it "should have the content 'Help page'" do
-    	visit help_path
-    	expect(page).to have_content ('Help page')
-
-    end
+    it { should have_content ('Help page')}
 
   end
+
 
 
   describe "About us" do
 
+    before {visit about_path}
 
+    it { should have_content ('About us')}
 
-    it "should have the content 'About us'" do
-    	visit about_path
-    	expect(page).to have_content ('About us')
-
-    end
   end
+
 
   describe "Contact page" do
 
-  
+    before {visit contact_path}  
 
-    it "should have the content 'Contact page'" do
-    	visit contact_path
-    	expect(page).to have_content ('Contact us')
-
-    end
+    it { should have_content ('Contact us')}
 
   end
 
